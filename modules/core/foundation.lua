98a3b2b969b77c2ae3e27726e3447e04eff35c8d
Alex Ellwein <alex.ellwein@gmail.com>
2015-10-18 20:33:05 +0200

    add more functions to foundation and more tests

--- modules/core/foundation.lua
+++ modules/core/foundation.lua
@@ -22,28 +22,41 @@ THE SOFTWARE.
 
 local foundation = {}
 
-foundation.isClass = function(self, t) 
-  if type(t) ~= "table" then return false end
-  if not t.super or type(super) ~= "function" then return false end
-  return true
-end
-
-foundation.instanceof = function(self, t)
-  assert(self:isClass(t), "is not a foundation class")
-end
-
 -- makes a table immutable
-foundation.immutable = function(self, t) 
+foundation.immutable = function(self, t)
   assert(type(t) == 'table', "must be of type table")
   local proxy = {}
   local mt = {
     __index = t,
+    __tostring = t.__tostring,
     __newindex = function(t,k,v)
-        error("attempt to modify an immutable object", 2)
-    end
+      error("attempt to modify an immutable object", 2)
+    end,
+    __metatable = false
   }
   setmetatable(proxy, mt)
   return proxy
+end
+
+-- foundation.Object is the root of all objects
+foundation.Object = {}
+foundation.Object.name = "foundation.Object"
+foundation.Object.super = function() return nil end -- no parent here
+foundation.Object.Object = function() end --default constructor, noop
+foundation.Object._mixins = {}
+foundation.Object.__tostring = function() return "foundation.Object()" end
+foundation.Object = foundation:immutable(foundation.Object)
+
+-- checks if argument is a foundation class
+foundation.isClass = function(self, t)
+  if type(t) ~= "table" then return false end
+  if not t.super or type(t.super) ~= "function" then return false end
+  return true
+end
+
+-- to check
+foundation.instanceof = function(self, t)
+  assert(self:isClass(t), "is not a foundation class")
 end
 
 -- class() prepares a class blueprint
@@ -51,14 +64,19 @@ foundation.class = function(self, name, base)
   assert(name,"name may not be nil")
   assert(string.len(name)>0, "name may not be empty")
   assert(string.sub(name,1,1) == string.upper(string.sub(name,1,1)), "name should have its first letter in upper case")
+  local clazz = {}
   if base then
     assert(self:isClass(base), "base should be a foundation class")
+    clazz.super = function() return base end
+  else
+    clazz.super = function() return self.Object end
   end
+  return clazz
 end
 
 -- new() creates a class instance, i.e. calls a constructor
 foundation.new = function(self, ...)
-end  
+end
 
 
 return foundation
--- spec/foundation_spec.lua
+++ spec/foundation_spec.lua
@@ -20,50 +20,101 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 ]]--
 
+require "busted.runner"()
 local foundation = require "modules.core.foundation"
 local assert = require("luassert")
 
-describe("foundation module", 
+describe("foundation module:",
   function()
-    
+  
+    -- foundation.Object
+    describe("foundation.Object",
+      function()
+
+        it("has defined name", 
+          function()
+            assert.True(foundation.Object.name == "foundation.Object")
+          end)
+
+        it("complies to isClass()",
+          function()
+            assert.True(foundation:isClass(foundation.Object))
+          end)
+
+        it("has no super",
+          function()
+            assert.True(type(foundation.Object.super) == "function")
+            assert.True(foundation.Object.super() == nil)
+          end)
+        
+        it("is not mutable",
+          function()
+            assert.has.error(function() foundation.Object.someFunc = function() end end, "attempt to modify an immutable object")
+          end)
+          
+        it("implements __tostring",
+          function()
+            assert.equals("foundation.Object()", tostring(foundation.Object))
+          end)
+          
+      end)
+  
+
     -- isClass()
     describe("isClass() function",
-    function()
-      it("returns false if argument is not a table",
       function()
-        assert.falsy(foundation:isClass(""))
-      end)
-  
-      it("returns false if argument is not a foundation class",
-      function()
-        assert.falsy(foundation:isClass({}))
-      end)
-    end)
-    
-    -- class()
-    describe("class() function", 
-    function()
       
-      it("disallows nil class name", 
-      function()
-        assert.has.errors(function() foundation:class(nil) end, "name may not be nil")
+        it("returns false if argument is not a table",
+          function()
+            assert.falsy(foundation:isClass(""))
+          end)
+
+        it("returns false if argument is not a foundation class",
+          function()
+            assert.falsy(foundation:isClass({}))
+          end)
+
+        it("returns true if argument is a foundation class",
+          function()
+            assert.True(foundation:isClass(foundation:class('MyClass')))
+          end)
+      
+        it("returns false if class has super as no function",
+          function()
+            assert.falsy(foundation:isClass({ ["super"] = "" }))
+          end)
+
+        it("returns true if class has super as function",
+          function()
+            assert.True(foundation:isClass({ ["super"] = function() end }))
+          end)
+      
       end)
-    
-      it("disallows empty class name",
+
+    -- class()
+    describe("class() function",
       function()
-        assert.has.errors(function() foundation:class("") end, "name may not be empty")
+      
+        it("disallows nil class name",
+          function()
+            assert.has.errors(function() foundation:class(nil) end, "name may not be nil")
+          end)
+
+        it("disallows empty class name",
+          function()
+            assert.has.errors(function() foundation:class("") end, "name may not be empty")
+          end)
+
+        it("disallows lowercased class names *by convention*",
+          function()
+            assert.has.errors(function() foundation:class("myClass") end, "name should have its first letter in upper case")
+          end)
+
+        it("disallows base arg of non class type",
+          function()
+            assert.has.errors(function() foundation:class("MyClass", {}) end, "base should be a foundation class")
+          end)
+      
       end)
-    
-      it("disallows lowercased class names *by convention*",
-      function()
-        assert.has.errors(function() foundation:class("myClass") end, "name should have its first letter in upper case")
-      end)
-    
-      it("disallows base arg of non class type",
-      function()
-        assert.has.errors(function() foundation:class("MyClass", {}) end, "base should be a foundation class")
-      end)
-    
-    end)
-  
+
   end)
